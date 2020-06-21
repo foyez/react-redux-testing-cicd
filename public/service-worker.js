190162eaf0a4b9bcfad184679934b8867408066b
foyez <foyezar@yahoo.com>
2020-06-21 14:10:48 +0600

    update service-worker.js & add manifest.json file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+// Help Link: https://googlechrome.github.io/samples/service-worker/custom-offline-page/
+// https://www.youtube.com/watch?v=IaJqMcOMuDM
+
 const CACHE_NAME = 'version-1'
 const urlsToCache = ['index.html', 'offline.html']
 
@@ -6,16 +9,53 @@ const self = this
 // INSTALL SW
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
       console.log('Opened cache')
 
       return cache.addAll(urlsToCache)
-    }),
+    })(),
+  )
+})
+
+// ACTIVATE THE SW
+self.addEventListener('activate', (event) => {
+  const cacheWhiteList = [CACHE_NAME]
+
+  event.waitUntil(
+    (async () => {
+      const cacheNames = await caches.keys()
+      await Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName)
+          }
+        }),
+      )
+    })(),
   )
 })
 
 // LISTEN FOR REQUESTS
-self.addEventListener('fetch', (e) => {})
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      (async () => {
+        try {
+          // First, try to use the navigation preload response if it's supported.
+          const preloadResponse = await event.preloadResponse
+          if (preloadResponse) {
+            return preloadResponse
+          }
 
-// ACTIVATE THE SW
-self.addEventListener('activate', (e) => {})
+          const networkResponse = await fetch(event.request)
+          return networkResponse
+        } catch (err) {
+          console.log('Fetch failed; returning offline page instead.', err)
+
+          return caches.match('offline.html')
+        }
+      })(),
+    )
+  }
+})
