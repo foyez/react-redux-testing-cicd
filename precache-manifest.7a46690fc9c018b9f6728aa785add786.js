4b59edc957c010a8b7efeb7f7c27005363f0f5b2
foyez <foyezar@yahoo.com>
2020-07-01 22:07:09 +0600

    Updates

---
+++ precache-manifest.7a46690fc9c018b9f6728aa785add786.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fda82cc7e148fa4669c72c54b93bea",
+    "url": "/react-redux-testing-cicd/index.html"
+  },
+  {
+    "revision": "f48889ed21f7da35ae5b",
+    "url": "/react-redux-testing-cicd/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "01979d6a4e06cb8003eb",
+    "url": "/react-redux-testing-cicd/static/css/main.ae97bdce.chunk.css"
+  },
+  {
+    "revision": "f48889ed21f7da35ae5b",
+    "url": "/react-redux-testing-cicd/static/js/2.5adede8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-testing-cicd/static/js/2.5adede8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01979d6a4e06cb8003eb",
+    "url": "/react-redux-testing-cicd/static/js/main.ef29b366.chunk.js"
+  },
+  {
+    "revision": "59acb5d4a425723d249b",
+    "url": "/react-redux-testing-cicd/static/js/runtime-main.9391391f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-redux-testing-cicd/static/media/SEGA.12a3db8b.woff"
+  }
+]);
